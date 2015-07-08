607a3c05ced457f06c92744b18f6c048f2e98d9e
Moritz Lawitschka <me@moritzlawitschka.de>
2015-07-08 14:26:20 +0200

    Initial Gem commit

---
+++ lib/jsonapi/resources/extensions.rb
@@ -0,0 +1,9 @@
+require "jsonapi/resources/extensions/version"
+
+module Jsonapi
+  module Resources
+    module Extensions
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jsonapi/resources/extensions/version.rb
@@ -0,0 +1,7 @@
+module Jsonapi
+  module Resources
+    module Extensions
+      VERSION = "0.1.0"
+    end
+  end
+end
